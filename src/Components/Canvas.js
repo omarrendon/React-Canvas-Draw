8536608a1db6e881d60281d0ac045115c4ba7d47
Omar <omarcr.96@gmail.com>
2020-05-10 00:24:36 -0500

    Modificacion de props desde inputs

--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -1,7 +1,11 @@
-import React, {useRef} from 'react'
+import React, {useRef, useState} from 'react'
 import CanvasDraw from "react-canvas-draw";
 
 const Canvas = () => {
+  const [brush, setBrush] = useState(1);
+  const [width, setWidth] = useState(400);
+  const [height, setHeight] = useState(400);
+
   const Draw = useRef(null);
   const secondDraw = useRef(null);
 
@@ -17,26 +21,44 @@ const Canvas = () => {
 
   const handleBack = () => {
     Draw.current.undo();
-  }
+  };
+
+  const brushSize = event => {
+    setBrush(parseInt(event.target.value, 10));
+  };
+
+  const widthSize = event => {
+    setWidth(parseInt(event.target.value, 10));
+  };
+
+  const heightSize = event => {
+    setHeight(parseInt(event.target.value, 10));
+  };
+
   return (
     <div>
+      <span>Tamaño de la brocha</span>
+      <input type="number" min={1} value={brush} onChange={brushSize} />
+
+      <span>Ancho</span>
+      <input type="number" min={50} value={width} onChange={widthSize} />
+
+      <span>Alto</span>
+      <input type="number" min={50} value={height} onChange={heightSize} />
       <button onClick={handleSave}> Guardar!</button>
       <button onClick={handleClear}> Limpiar </button>
       <button onClick={handleBack}> Atras</button>
+
       <CanvasDraw
-        brushRadius={5}
+        brushRadius={brush}
         brushColor={"red"}
-        canvasWidth={600}
-        canvasHeight={200}
+        canvasWidth={width}
+        canvasHeight={height}
         hideGrid={false}
         ref={Draw}
       />
       <hr />
-      <CanvasDraw 
-        hideGrid={true} 
-        disabled={true} 
-        ref={secondDraw} 
-      />
+      <CanvasDraw hideGrid={true} disabled={true} ref={secondDraw} />
     </div>
   );
 }
